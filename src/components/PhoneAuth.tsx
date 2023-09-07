c669d4d89f50aecb85f1d1c55c06a99c3770b979
annasavinskaya <savinskaya.ann@gmail.com>
2023-09-07 11:06:05 +0200

    sign up screen

--- src/components/PhoneAuth.tsx
+++ src/components/PhoneAuth.tsx
@@ -1,11 +1,15 @@
 import { FC, useState } from 'react';
 import { StyledPhoneAuth } from '../styles/components/PhoneAuth';
-import { Link } from "react-router-dom";
-import PhoneInput from 'react-phone-number-input'
-
-import 'react-phone-number-input/style.css'
-import { Button } from './Button';
+import { Link } from 'react-router-dom';
 import { Field, Formik, Form } from 'formik';
+import PhoneInput from 'react-phone-number-input';
+import DatePicker from 'react-datepicker';
+import Select from 'react-select';
+
+import { Button } from './Button';
+
+import 'react-phone-number-input/style.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 interface Props {
   title: string;
@@ -15,17 +19,27 @@ interface Props {
   stepsCount: number;
 }
 
-export const PhoneAuth: FC<Props> = ({
-  title,
-  subtitle,
-  subtitleLinkText,
-  subtitleLinkPath,
-  stepsCount,
-}) => {
+const genderList = [
+  {
+    value: 'Male',
+    label: 'Male',
+  },
+  {
+    value: 'female',
+    label: 'female',
+  },
+  {
+    value: 'other',
+    label: 'other',
+  },
+];
+
+export const PhoneAuth: FC<Props> = ({ title, subtitle, subtitleLinkText, subtitleLinkPath, stepsCount }) => {
   const [value, setValue] = useState();
   const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [startDate, setStartDate] = useState<Date | null>();
 
-  const handleNext = () => {
+  const handleLoginNext = () => {
     setCurrentIndex(1);
   };
 
@@ -33,24 +47,32 @@ export const PhoneAuth: FC<Props> = ({
     setCurrentIndex(1);
   };
 
+  const handleLogin = () => {};
+
+  const handleResendCode = () => {};
+
+  const handleNextCreateAcc = () => {
+    setCurrentIndex(2);
+  };
+
+  const handleSubmitCreateAcc = () => {};
+
   return (
     <StyledPhoneAuth>
       <div>
         <h4>{title}</h4>
-        <p className='subtitle'>
+        <p className="subtitle">
           <span>{subtitle}</span>
           <Link to={subtitleLinkPath}>{subtitleLinkText}</Link>
         </p>
       </div>
       {currentIndex === 0 ? (
         <div>
-          <div className='label-wrapper'>
-            <p className='label'>Phone number</p>
-            {false && (
-              <p className='error'>Phone number is required</p>
-            )}
+          <div className="label-wrapper">
+            <p className="label">Phone number</p>
+            {false && <p className="error">Phone number is required</p>}
           </div>
-          
+
           <PhoneInput
             international
             placeholder="Enter phone number"
@@ -59,43 +81,89 @@ export const PhoneAuth: FC<Props> = ({
             onChange={() => setValue}
             smartCaret={false}
           />
-
-          <Button type="button" onClick={stepsCount === 2 ? handleNext : handleCreateAccount}>
+          {stepsCount === 3 && (
+            <div className="agreement-wrapper">
+              <p className="agreement-text">
+                By clicking “Create account” you agree to <Link to="/">Terms & Conditions</Link> and{' '}
+                <Link to="/">Privacy Policy</Link>
+              </p>
+            </div>
+          )}
+          <Button type="button" onClick={stepsCount === 2 ? handleLoginNext : handleCreateAccount}>
             {stepsCount === 2 ? 'Next' : 'create account'}
           </Button>
         </div>
-      ) : (
-        <Formik
-          initialValues={{code: ''}}
-          onSubmit={() => {}}
-        >
-          {({ handleSubmit }) => (
-            <Form className='form'>
-              <div className='label-wrapper'>
-                <p className='label'>Verification code from SMS</p>
-                {false && (
-                  <p className='error'>Code is required</p>
-                )}
+      ) : currentIndex === 1 ? (
+        <Formik initialValues={{ code: '' }} onSubmit={() => {}}>
+          {() => (
+            <Form className="form">
+              <div className="label-wrapper">
+                <p className="label">Verification code from SMS</p>
+                {false && <p className="error">Code is required</p>}
               </div>
-              
-              <Field
-                name="code"
-                type="text"
-                className="input-text"
-                placeholder="Enter Verification code"
+
+              <Field name="code" type="text" className="input-text" placeholder="Enter Verification code" />
+              <Button type="button" onClick={stepsCount === 2 ? handleLogin : handleNextCreateAcc}>
+                {stepsCount === 2 ? 'Log in' : 'next'}
+              </Button>
+              <Button flat textButton type="button" onClick={handleResendCode} className="text-button">
+                resend code
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      ) : (
+        <Formik initialValues={{ firstName: '', lastName: '', birthday: '', gender: '' }} onSubmit={() => {}}>
+          {({ values, setFieldValue }) => (
+            <Form className="form">
+              <div className="label-wrapper">
+                <p className="label">first name</p>
+                {false && <p className="error">First name is required</p>}
+              </div>
+              <Field name="firstName" type="text" className="input-text" placeholder="Enter first name" />
+              <div className="label-wrapper">
+                <p className="label">last name</p>
+                {false && <p className="error">Last name is required</p>}
+              </div>
+              <Field name="lastName" type="text" className="input-text" placeholder="Enter last name" />
+              <div className="label-wrapper">
+                <p className="label">your birthday</p>
+                {false && <p className="error">Birthday is required</p>}
+              </div>
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                placeholderText="date of birth"
+                //It should be added, because  it is not convenient to choose birthday by flipping through just one month.
+                // showMonthDropdown
+                // showYearDropdown
+                dropdownMode="select"
               />
-              <Button type="button" onClick={handleSubmit}>
-                Log in
+              <div className="label-wrapper">
+                <p className="label">your gender</p>
+                {false && <p className="error">Gender is required</p>}
+              </div>
+              <Select
+                name="gender"
+                placeholder="select gender"
+                options={genderList}
+                value={genderList.find((item) => item.value === values.gender)}
+                onChange={(selectedOption) => setFieldValue('gender', selectedOption?.value)}
+                className="gender-select"
+                classNamePrefix="gender-select"
+                isSearchable={false}
+              />
+              <Button type="button" onClick={handleSubmitCreateAcc}>
+                submit
               </Button>
             </Form>
           )}
         </Formik>
       )}
-      
+
       <div>
         {Array.from({ length: stepsCount }, (_, index) => (
-          <div key={index} className={`tile ${currentIndex === index ? 'active' : ''}`}>
-          </div>
+          <div key={index} className={`tile ${currentIndex === index ? 'active' : ''}`}></div>
         ))}
       </div>
     </StyledPhoneAuth>
